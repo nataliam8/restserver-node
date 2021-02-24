e06a763c1970fbd0546c99bde9d517cac25d4a5a
Natalia Meneses <nmeneses8@gmail.com>
2021-02-24 09:52:01 -0500

    file upload agregado

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 
 const { dbConnection } = require('../database/config');
 
@@ -14,6 +15,7 @@ class Server {
             buscar:     '/api/buscar',
             categorias: '/api/categorias',
             productos:  '/api/productos',
+            uploads:    '/api/uploads',
             usuarios:   '/api/usuarios',
         }
 
@@ -39,6 +41,13 @@ class Server {
 
         // Directorio público
         this.app.use( express.static('public') );
+
+        // fileUpload - Carga de archivos
+        this.app.use(fileUpload({
+            useTempFiles : true,
+            tempFileDir : '/tmp/',
+            createParentPath: true,
+        }));
     }
 
     routes() {
@@ -46,6 +55,7 @@ class Server {
         this.app.use(this.paths.buscar, require('../routes/buscar'));
         this.app.use(this.paths.categorias, require('../routes/categorias'));
         this.app.use(this.paths.productos, require('../routes/productos'));
+        this.app.use(this.paths.uploads, require('../routes/uploads'));
         this.app.use(this.paths.usuarios, require('../routes/usuarios'));
     }
 
